26869c5e88438b63779cd625171c3ceb7b93ad71
linx <linx@haiwaigo.cn>
2017-02-27 21:08:02 +0800

    responsebody配置

--- src/main/java/com/ygjt/gdn/psp/controller/GoodsController.java
+++ src/main/java/com/ygjt/gdn/psp/controller/GoodsController.java
@@ -1,29 +1,33 @@
 package com.ygjt.gdn.psp.controller;
 
+import com.ygjt.gdn.psp.controller.common.ResultFactory;
+import com.ygjt.gdn.psp.controller.common.model.ResultData;
 import com.ygjt.gdn.psp.controller.vo.po.GoodsPO;
 import com.ygjt.gdn.psp.exception.ServiceException;
 import com.ygjt.gdn.psp.model.GoodsDo;
 import com.ygjt.gdn.psp.model.GoodsQuery;
-import com.ygjt.gdn.psp.model.UserDo;
 import com.ygjt.gdn.psp.service.GoodsService;
 import com.ygjt.gdn.psp.utils.SPage;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by linx on 2017-01-02.
  */
 @Controller
 @RequestMapping("/goods")
-public class GoodsController extends BaseController{
+public class GoodsController{
     @Autowired
     GoodsService goodsService;
 
@@ -39,4 +43,11 @@ public class GoodsController extends BaseController{
         model.addAttribute("goodsPO", goodsPO);
         return new ModelAndView("goods/goodsList");
     }
+
+    @RequestMapping("test")
+    @ResponseBody
+    public ResultData<?> test(){
+        return ResultFactory.createSuccessResultData(null);
+    }
+
 }
